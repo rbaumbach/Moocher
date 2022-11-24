6b779f6d350730179c5fa4426b05a59073a92133
Ryan Baumbach <github@ryan.codes>
2022-11-24 10:09:33 -0700

    Fix imports

--- MoocherPolling/PollingMatcherEngine.swift
+++ MoocherPolling/PollingMatcherEngine.swift
@@ -20,7 +20,7 @@
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //SOFTWARE.
 
-import XCTest
+import Foundation
 
 public struct PollingMatcherEngine<T> {
     // MARK: - Readonly properties
